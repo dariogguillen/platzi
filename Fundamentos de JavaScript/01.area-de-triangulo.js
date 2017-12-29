370c004b8639622dc18f8b9a519a4fee2ec66bca
dariogguillen <dar_fe@hotmail.com>
2017-12-29 15:13:37 -0600

    comenzando curso Fundamentos de JavaScript

---
+++ Fundamentos de JavaScript/01.area-de-triangulo.js
@@ -0,0 +1,12 @@
+let base = 5
+let height = 7
+const trianguloArea = (base, height) => base * height / 2
+
+console.log(`El area de un triangulo de base
+${base} y altura ${height} es: ${trianguloArea(base, height)}`)
+
+const PI = 3.1416
+let radio = 5
+const areaCirculo = (PI , radio) => PI * (radio*radio)
+console.log(`El area de un circulo de radio ${radio}
+es igual a ${areaCirculo(PI, radio)}`)
---
+++ Fundamentos de JavaScript/02.ver-starwars7.js
@@ -0,0 +1,36 @@
+const starWars7 = 'Star Wars: El Despertar de la Fuerza'
+const pgStarWars7 = 13
+
+const nameDario = 'Dario'
+const ageDario = 28
+
+const nameDana = 'Dana'
+const ageDana = 12
+
+const puedeVerStarWars7 = (name, age, isWithAdult = false) => {
+    if (age >= pgStarWars7) {
+    alert(`${name} puede pasar a ver ${starWars7}`)
+    } else if (isWithAdult) {
+    alert(`${name} puede pasar a ver ${starWars7}.
+    Aunque su edad es ${age}, viene acompanado/a por un adulto `)
+    } else {
+    alert(`${name} no puede pasar a ver ${starWars7}.
+    Tiene ${age} anos y necesita tener ${pgStarWars7}.`)
+    }
+}
+
+puedeVerStarWars7(nameDario, ageDario)
+puedeVerStarWars7(nameDana, ageDana, true)
+
+
+/* function puedeVerStarWars7(name, age, isWithAdult = false) {
+    if (age >= pgStarWars7){
+        alert(`${name} puede pasar a ver ${starWars7}`)
+    } else if (isWithAdult){
+        alert(`${name} puede pasar a ver ${starWars7}.
+        Aunque su edad es ${age}, viene acompanado/a por un adulto `)
+    } else{
+        alert(`${name} no puede pasar a ver ${starWars7}.
+        Tiene ${age} anos y necesita tener ${pgStarWars7}.`)
+    }
+} */
